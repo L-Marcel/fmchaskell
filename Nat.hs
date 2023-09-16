2799f287360de02a066863593cacaeb48757fa3a
l-marcel <lmgh1312@gmail.com>
2023-09-16 17:10:59 -0300

    Adicionando comentários e definições das funções gcd (mdc) e lcm (mmc)

--- Nat.hs
+++ Nat.hs
@@ -1,49 +1,59 @@
 module Nat where
 import Prelude 
-  hiding ((+), (*), (^), quot, min, div, max, pred, rem)
+  hiding ((+), (*), (^), quot, min, gcd, lcm, div, max, pred, rem)
 
 data Nat = O | S Nat
   deriving (Eq , Show)
 
+-- adição
 (+) :: Nat -> Nat -> Nat
 (+) m O = m
 (+) m (S n) = S (m + n)
-  
+
+-- multiplicação
 (*) :: Nat -> Nat -> Nat
 (*) m O = O
 (*) m (S n) = m + (m * n)
 
+-- exponenciação
 (^) :: Nat -> Nat -> Nat
 (^) m O = (S O)
 (^) m (S n) = m * (m ^ n)
 
+-- dobro
 double :: Nat -> Nat
 double O = O
 double (S n) = S (S (double n))
 
+-- predecessor
 pred :: Nat -> Nat
 pred O = O
 pred (S m) = m
 
+-- fatorial
 fact :: Nat -> Nat
 fact O = (S O)
 fact (S m) = (S m) * (fact m)
 
+-- fibonacci
 fib :: Nat -> Nat
 fib O = O
 fib (S O) = (S O)
 fib (S (S m)) = fib (S m) + fib m
 
+-- valor mínimo
 min :: Nat -> Nat -> Nat
 min O m = O
 min m O = O
 min (S m) (S n) = S (min m n)
 
+-- valor máximo
 max :: Nat -> Nat -> Nat
 max O m = m
 max m O = m
 max (S m) (S n) = S (max m n)
 
+-- quociente
 quot :: Nat -> Nat -> Nat
 quot m n = quot' m n n
   where
@@ -53,6 +63,7 @@ quot m n = quot' m n n
     quot' m O k = S (quot' m k k)
     quot' O m k = O
 
+-- resto
 rem :: Nat -> Nat -> Nat
 rem O n = O
 rem (S m) O = S (rem m O)
@@ -62,11 +73,25 @@ rem m n = rem' m (n * (quot m n))
     rem' (S m) (S n) = rem' m n
     rem' m O = m
 
+-- (quociente, resto)
 div :: Nat -> Nat -> (Nat, Nat)
 div m n = (quot m n, rem m n)
 
--- gcd :: Nat -> Nat -> Nat
--- mdc
+-- máximo divisor comum
+gcd :: Nat -> Nat -> Nat
+gcd m n = gcd' m n (S O) (S (min m n))
+  where
+    gcd' :: Nat -> Nat -> Nat -> Nat -> Nat
+    gcd' m n p (S O) = S O
+    gcd' m n O q = q
+    gcd' m n p (S q) = gcd' m n (max (rem m q) (rem n q)) q
 
--- lcm :: Nat -> Nat -> Nat
--- mmc
+-- mínimo multiplo comum
+lcm :: Nat -> Nat -> Nat
+lcm m n = lcm' m n (S O) (pred (max m n))
+  where 
+    lcm' :: Nat -> Nat -> Nat -> Nat -> Nat
+    lcm' m n O q = q
+    lcm' O n p q = O
+    lcm' m O p q = O
+    lcm' m n p q = lcm' m n (max (rem (S q) m) (rem (S q) n)) (S q)
